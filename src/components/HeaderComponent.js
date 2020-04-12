6f7c8c9e01cf877aece46f28b26ee54204cf3924
Lucas Soares <lucassoares-2010@hotmail.com>
2020-04-12 16:39:09 -0300

    Uncontrolled forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, Jumbotron } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Jumbotron, Button, Modal, Form } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
+    }
+
+    toggleModal = () => { this.setState({ isModalOpen: !this.state.isModalOpen }) }
+    handleLogin = (event) => {
+        this.toggleModal();
+        alert("Username: " + this.username.value + "\nPassword: " +
+            this.password.value + "\nRemember: " + this.remember.checked);
+        event.preventDefault();            
+    }
 
     render() {
         return (
@@ -36,6 +51,13 @@ class Header extends Component {
                                 </NavLink>
                             </NavItem>
                         </Nav>
+                        <Nav className="ml-auto">
+                            <Nav.Item>
+                                <Button outline onClick={this.toggleModal}>
+                                    <span className="fa fa-sign-in fa-lg" /> Login
+                                </Button>
+                            </Nav.Item>
+                        </Nav>
                     </Navbar.Collapse>
                 </Navbar>
                 <Jumbotron>
@@ -50,6 +72,31 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal show={this.state.isModalOpen} onHide={this.toggleModal}>
+                    <Modal.Header closeButton>Login</Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={this.handleLogin}>
+                            <Form.Group>
+                                <Form.Label htmlFor="username">Username</Form.Label>
+                                <Form.Control type="text" id="username" name="username"
+                                    ref={(input) => this.username = input} />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="password">Password</Form.Label>
+                                <Form.Control type="password" id="password" name="password"
+                                    ref={(input) => this.password = input} />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Check>
+                                    <Form.Check.Input type="checkbox" name="remember"
+                                        ref={(input) => this.remember = input} />
+                                    <Form.Check.Label>Remember me</Form.Check.Label>
+                                </Form.Check>
+                            </Form.Group>
+                            <Button type="submit" value="submit" color="bg-primary">Login</Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
             </>
         )
     }
